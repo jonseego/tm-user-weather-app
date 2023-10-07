9ea55de6ff135b10f8b0385bf07a5389662f8e1c
Jonathan A. See Go <jonathanseego@yahoo.com>
2023-10-07 20:27:22 +0800

    added periodic update in temperature

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Subject, take } from 'rxjs';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Subject, take, takeUntil, timer } from 'rxjs';
 
 import { User, WeatherResponse } from '../models';
 
@@ -11,20 +11,30 @@ import { StorageService } from '../services/storage.service';
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss'],
 })
-export class UserCardComponent implements OnInit {
+export class UserCardComponent implements OnInit, OnDestroy {
   @Input({ required: true }) user: User;
   @Input({ required: true }) canSave: boolean;
 
   weather$ = new Subject<WeatherResponse>();
   weatherIconSrc: string;
 
+  private updateIntervalMs = 5 * 60 * 1000; // 5 mins
+  private destroy$ = new Subject<void>();
+
   constructor(private weatherApiService: WeatherApiService, private storageService: StorageService) {}
 
   ngOnInit() {
-    this.weatherApiService.getWeather(this.user.location.coordinates).pipe(take(1)).subscribe((weather) => {
-      this.weather$.next(weather);
-      this.weatherIconSrc = this.mapWeatherCodeToImageSource(weather.current_weather.weathercode);
+    timer(0, this.updateIntervalMs).pipe(takeUntil(this.destroy$)).subscribe(() => {
+      this.weatherApiService.getWeather(this.user.location.coordinates).pipe(take(1)).subscribe((weather) => {
+        this.weather$.next(weather);
+        this.weatherIconSrc = this.mapWeatherCodeToImageSource(weather.current_weather.weathercode);
+      });
     });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   saveUserInfo() {
